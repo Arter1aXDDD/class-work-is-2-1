6852fa5f5b9c9ffab538a7a44db4db26c93cfc5c
Arter1aXDDD <steepnikita64@gmail.com>
2024-02-05 15:29:09 +0300

    Выполнили работу с объектами

--- task_3/main.js
+++ task_3/main.js
@@ -1,23 +1,15 @@
-//const students = ["Андрей", "Иван", "Стас"];
-//students[50] = "Индекс 50";
-//console.log(students);
+let college = [
+    { name: "Tom", age: 17, group: "ИС-2-2" },
+    { name: "Bob", age: 13, group: "ИС-2-1" },
+    { name: "Stan", age: 26, group: "ИС-2-2" },
+    { name: "Harry", age: 20, group: "ИС-4-1" },
+];
 
-
-//let college = [ ["Андрей", "Иван", "Стас"], [205, 111, 304] ];
-//college[1][1] = 112
-//let kab = college[1][1]
-//console.log(kab);
-
-let number = [ 1, 2, 3, 4, 5, 6, 7 ]
-
-const countLenght = 9;
-
-if ( number.length === countLenght ) {
-    console.log("ok");
-} else if ( number.length < countLenght ) {
-    let result = countLenght - number.length;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно увеличить до ${countLenght}. Прибавьте к массиву значение ${result}`);
-} else if ( number.length > countLenght ) { 
-    result = number.length - countLenght;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно уменьшить до ${countLenght}. Удалите из массива значение ${result}`)
-}                                                       
+let count = 2
+let studentAge = college[count]["age"]
+let studentName = college[count]["name"]
+if ( studentAge >= 18 ) {
+    console.log(`Студент ${studentName} совершеннолетний. Ему ${studentAge} лет`);
+} else if ( studentAge < 18) {
+    console.log(`Студент ${studentName} не совершеннолетний. Ему ${studentAge} лет`); 
+}
